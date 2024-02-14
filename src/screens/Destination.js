381cded1316dae479165c87bed2dc6b31cb1f69e
Ayaan22 <pathan.gouse77@gmail.com>
2024-02-14 23:23:27 +0530

    captain implementation

--- src/screens/Destination.js
+++ src/screens/Destination.js
@@ -1,14 +1,44 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Alert, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {apiKey} from '../utils/apiKey';
+import { getDistance } from 'geolib';
 
-const Destination = ({navigation}) => {
+const Destination = ({navigation, route}) => {
+  const userLocation=route?.params?.userLocation
+  const {latitude,longitude}=userLocation
+  console.log(latitude,longitude,'from origin');
+
+const handlePlaceClick=(data,details=null)=>{
+const {geometry}=details
+if (geometry&&geometry.location) {
+  const {lat:destLat,lng:destLong}=geometry.location
+  const distance=getDistance({latitude:latitude,longitude:longitude},{
+    latitude:destLat,longitude:destLong
+  })
+  const fromMetersToKms=distance/1000
+  console.log(fromMetersToKms,'kms');
+  if (fromMetersToKms<=50) {
+    navigation.navigate('Home', {details,fromMetersToKms});
+  }else{
+    Alert.alert("The destination is too far away.")
+  }
+} 
+
+}
+
+
+
   return (
     <View style={{flex: 1, padding: 20, gap: 20}}>
       <View style={{flexDirection: 'row', alignItems: 'center'}}>
-        <Ionicons name="chevron-back" size={25} color="black" onPresS={()=>navigation.goBack()} />
+        <Ionicons
+          name="chevron-back"
+          size={25}
+          color="black"
+          onPresS={() => navigation.goBack()}
+        />
         <Text
           style={{color: 'black', fontSize: 20, textAlign: 'center', flex: 1}}>
           Select Destination
@@ -16,10 +46,10 @@ const Destination = ({navigation}) => {
       </View>
       <GooglePlacesAutocomplete
         placeholder="Search"
-        onPress={(data, details = null) => {
-        navigation.navigate('Home',{details})
-      
-        }}
+        onPress={handlePlaceClick}
+        // onPress={(data, details = null) => {
+        //   navigation.navigate('Home', {details});
+        // }}
         query={{
           key: apiKey,
           language: 'en',
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -7,23 +7,39 @@ import {
   View,
 } from 'react-native';
 import React, {useEffect, useState, useRef} from 'react';
-import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE, Marker, Polyline} from 'react-native-maps';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Entypo from 'react-native-vector-icons/Entypo';
 import GetLocation from 'react-native-get-location';
 import axios from 'axios';
 import {apiKey} from '../utils/apiKey';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import {getDistance} from 'geolib';
 
 const Home = ({navigation, route}) => {
   const destination = route?.params?.details;
+  const fromMetersToKms = route?.params?.fromMetersToKms;
   const geometry = destination?.geometry;
   const location = geometry?.location;
   const latitude = location?.lat;
   const longitude = location?.lng;
 
-  const formatted_address = destination?.formatted_address;
+  const [isRideSelected, setisRideSelected] = useState(false);
+  const [captain, setcaptain] = useState([]);
+  const [rideData, setrideData] = useState([]);
 
-  console.log(latitude, longitude, formatted_address, '--from home');
+  console.log(captain, '----riderr');
+
+  // Generate 5 persons data around the given latitude and longitude
+  const captainData = [
+    {id: 1, lat: 37.3821571, long: -122.0923715, name: 'Person 1'},
+    {id: 2, lat: 37.3887415, long: -122.08536930000001, name: 'Person 2'},
+    {id: 3, lat: 37.3827878, long: -122.077937, name: 'Person 3'},
+    {id: 4, lat: 37.3782852, long: -122.0866501, name: 'Person 4'},
+    {id: 5, lat: 37.3770582, long: -122.0816042, name: 'Person 5'},
+  ];
+
+  const formatted_address = destination?.formatted_address;
 
   const mapRef = useRef();
   const [userLocation, setuserLocation] = useState({
@@ -31,6 +47,14 @@ const Home = ({navigation, route}) => {
     longitude: -122.4324,
   });
   const [origin, setorigin] = useState(null);
+
+  const coordinates = [
+    {latitude: userLocation.latitude, longitude: userLocation.longitude},
+    {
+      latitude: latitude,
+      longitude: longitude,
+    },
+  ];
 
   useEffect(() => {
     const getLocation = () => {
@@ -40,7 +64,6 @@ const Home = ({navigation, route}) => {
         maximumAge: 10000,
       })
         .then(async location => {
-        
           setuserLocation({
             latitude: location.latitude,
             longitude: location.longitude,
@@ -58,7 +81,10 @@ const Home = ({navigation, route}) => {
           const {data} =
             await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.latitude},${location.longitude}&key=${apiKey}
           `);
-          console.log(data.results[0].formatted_address,'--from current location');
+          console.log(
+            data.results[0].formatted_address,
+            '--from current location',
+          );
           setorigin(data.results[0].formatted_address);
         })
         .catch(error => {
@@ -80,11 +106,37 @@ const Home = ({navigation, route}) => {
       ];
 
       mapRef.current.fitToCoordinates(coordinates, {
-        edgePadding: {top: 50, right: 50, bottom: 50, left: 50},
+        edgePadding: {top: 200, right: 50, bottom: 50, left: 50},
         animated: true,
       });
     }
   }, [latitude, longitude, userLocation]);
+
+  const handleGetCaptain = ({title, getRideModePrice}) => {
+    setrideData({title: title, getRideModePrice: getRideModePrice});
+
+    let closedCaptain = null;
+    let minDistance = Infinity;
+    captainData.forEach(captain => {
+      const distacne = getDistance(
+        {latitude: captain.lat, longitude: captain.long},
+        {latitude: userLocation?.latitude, longitude: userLocation?.longitude},
+      );
+      if (distacne < minDistance) {
+        minDistance = distacne;
+        closedCaptain = captain;
+      }
+    });
+
+    setisRideSelected(true);
+    setcaptain(closedCaptain);
+    console.log(closedCaptain, '---->captain');
+  };
+
+  const getOpt = () => {
+    const otp = Math.floor(Math.random() * 9000) + 1000;
+    return otp.toString();
+  };
 
   return (
     <View style={{flex: 1, backgroundColor: 'white'}}>
@@ -109,6 +161,18 @@ const Home = ({navigation, route}) => {
             }}
           />
 
+          {captainData &&
+            captainData.map((item, index) => (
+              <Marker
+                key={index}
+                coordinate={{
+                  latitude: item?.lat,
+                  longitude: item?.long,
+                }}
+                pinColor="yellow"
+              />
+            ))}
+
           {longitude && latitude && (
             <Marker
               coordinate={{
@@ -116,6 +180,14 @@ const Home = ({navigation, route}) => {
                 longitude: longitude,
               }}
               pinColor="green"
+            />
+          )}
+
+          {longitude && latitude && (
+            <Polyline
+              coordinates={coordinates}
+              strokeColor="black" // fallback for when `strokeColors` is not supported by the map-provider
+              strokeWidth={1.7}
             />
           )}
         </MapView>
@@ -167,7 +239,7 @@ const Home = ({navigation, route}) => {
       {/* ///Second Box/// */}
       <View
         style={{
-          flex: 0.3,
+          flex: 0.35,
           padding: 20,
         }}>
         <View
@@ -177,7 +249,7 @@ const Home = ({navigation, route}) => {
             borderRadius: 25,
           }}>
           <TouchableOpacity
-            onPress={() => navigation.navigate('Destination')}
+            onPress={() => navigation.navigate('Destination', {userLocation})}
             style={{
               backgroundColor: 'white',
               margin: 10,
@@ -196,6 +268,72 @@ const Home = ({navigation, route}) => {
               editable={false}
             />
           </TouchableOpacity>
+
+          {/* ////Ride Options /// */}
+
+          {longitude && latitude && !isRideSelected && (
+            <View style={{}}>
+              <RideOption
+                icon={'bicycle'}
+                title={'Bike'}
+                fromMetersToKms={fromMetersToKms}
+                onPress={handleGetCaptain}
+              />
+              <RideOption
+                icon={'car-sport-sharp'}
+                title={'Car'}
+                fromMetersToKms={fromMetersToKms}
+                onPress={handleGetCaptain}
+              />
+              <RideOption
+                icon={'car'}
+                title={'Auto'}
+                fromMetersToKms={fromMetersToKms}
+                onPress={handleGetCaptain}
+              />
+            </View>
+          )}
+
+          {isRideSelected && (
+            <View>
+              <Text
+                style={{
+                  textAlign: 'center',
+                  fontSize: 17,
+                  color: 'black',
+                  fontWeight: '800',
+                }}>
+                Captain name: {captain.name}
+              </Text>
+              <Text
+                style={{
+                  textAlign: 'center',
+                  fontSize: 17,
+                  color: 'black',
+                  fontWeight: '800',
+                }}>
+                Ride Mode: {rideData?.title}
+              </Text>
+              <Text
+                style={{
+                  textAlign: 'center',
+                  fontSize: 17,
+                  color: 'black',
+                  fontWeight: '800',
+                }}>
+                Ride Price: $ {rideData?.getRideModePrice()}
+              </Text>
+              <Text
+                style={{
+                  textAlign: 'center',
+                  fontSize: 17,
+                  color: 'black',
+                  fontWeight: '800',
+                }}>
+                Ride OTP: {getOpt()}
+              </Text>
+            </View>
+          )}
         </View>
       </View>
     </View>
@@ -205,3 +343,42 @@ const Home = ({navigation, route}) => {
 export default Home;
 
 const styles = StyleSheet.create({});
+
+const RideOption = ({title, onPress, icon, fromMetersToKms}) => {
+  const getRideModePrice = () => {
+    if (title == 'Bike') {
+      return Math.round(9 * fromMetersToKms);
+    } else if (title == 'Car') {
+      return Math.round(25 * fromMetersToKms);
+    } else {
+      return Math.round(20 * fromMetersToKms);
+    }
+  };
+  return (
+    <TouchableOpacity
+      onPress={() =>
+        onPress({title: title, getRideModePrice: getRideModePrice})
+      }
+      style={{
+        backgroundColor: 'white',
+        marginHorizontal: 10,
+        marginVertical: 5,
+        borderRadius: 5,
+        padding: 15,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      }}>
+      <View style={{flexDirection: 'row', alignItems: 'center', gap: 10}}>
+        <Ionicons name={icon} size={20} color="black" />
+        <Text style={{color: 'black', fontSize: 15, fontWeight: '600'}}>
+          {title}
+        </Text>
+      </View>
+
+      <Text style={{color: 'black', fontSize: 15, fontWeight: '600'}}>
+        ₹ {getRideModePrice()}
+      </Text>
+    </TouchableOpacity>
+  );
+};
